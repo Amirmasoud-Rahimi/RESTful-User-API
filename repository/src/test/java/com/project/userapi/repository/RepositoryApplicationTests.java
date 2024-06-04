73d5d889718faae9771a95d433a5b1e448137ef0
Amirmasoud-Rahimi <amirmasoudrahimiofficial@gmail.com>
2024-06-04 17:33:56 +0330

    start

---
+++ repository/src/test/java/com/project/userapi/repository/RepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.userapi.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/project/restful_user_api/RestfulUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restful_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
